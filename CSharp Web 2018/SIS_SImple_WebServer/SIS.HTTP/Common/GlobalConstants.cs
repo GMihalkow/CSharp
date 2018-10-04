87d37563ea8d09f6c57265b6a65e4249f203205b
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2018-10-04 13:17:19 +0300

    change

--- CSharp Web 2018/SIS_SImple_WebServer/SIS.HTTP/Common/GlobalConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SIS.HTTP.Common
-{
-    public class GlobalConstants
-    {
-        public const string HttpOneProtocolFragment = "HTTP/1.1";
-
-        public const string HostHeaderKey = "Host";
-    }
-}
