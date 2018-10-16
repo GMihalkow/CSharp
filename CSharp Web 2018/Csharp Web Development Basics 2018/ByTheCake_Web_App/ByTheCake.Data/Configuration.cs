65abe984fad89e778dd2c7810b0be42773b3dbd6
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2018-10-16 13:36:10 +0300

    change

--- CSharp Web 2018/Csharp Web Development Basics 2018/ByTheCake_Web_App/ByTheCake.Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ByTheCake.Data
-{
-    public class Configuration
-    {
-        public const string ConnectionString = @"Server=DESKTOP-9SI8PGG\SQLEXPRESS;Database=ByTheCakeDb;Trusted_Connection=true";
-    }
-}
