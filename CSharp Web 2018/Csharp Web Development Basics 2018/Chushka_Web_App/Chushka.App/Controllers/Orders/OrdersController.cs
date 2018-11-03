886d76284025407b6558920f34b271b9e943c14a
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2018-11-03 13:15:12 +0200

    Order products implemented and view all orders.

---
+++ CSharp Web 2018/Csharp Web Development Basics 2018/Chushka_Web_App/Chushka.App/Controllers/Orders/OrdersController.cs
@@ -0,0 +1,53 @@
+﻿namespace Chushka.App.Controllers.Orders
+{
+    using Chushka.App.Models;
+    using Chushka.App.Models.Enums;
+    using Chushka.App.ViewModels.Orders;
+    using SIS.HTTP.Responses;
+    using SIS.MvcFramework;
+    using System;
+    using System.Linq;
+
+    public class OrdersController : BaseController
+    {
+        [Authorize(nameof(UserRole.Admin))]
+        public IHttpResponse All()
+        {
+            var orders =
+                this.DbContext
+                .Orders
+                .Select(o => new AllOrdersViewModel
+                {
+                    Id = o.Id,
+                    Customer = o.Client.Username,
+                    Product = o.Product.Name,
+                    OrderedOn = o.OrderedOn
+                })
+                .ToArray();
+
+            return this.View(orders);
+        }
+
+        [Authorize]
+        public IHttpResponse Order(int id)
+        {
+            var product = this.DbContext.Products.FirstOrDefault(p => p.Id == id);
+            var user = this.DbContext.Users.FirstOrDefault(p => p.Username == this.User.Username);
+            if (product == null)
+                return this.BadRequestError("Product doesn't exist");
+            if (user == null)
+                return this.Redirect("/users/login");
+
+            Order order = new Order()
+            {
+                ClientId = user.Id,
+                ProductId = id
+            };
+
+            this.DbContext.Orders.Add(order);
+            this.DbContext.SaveChanges();
+
+            return this.Redirect("/");
+        }
+    }
+}
