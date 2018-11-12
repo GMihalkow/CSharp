8d9331e1cd15b095caa028623a849310fa09e171
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2018-11-12 15:56:26 +0200

    Implemented db layer.

---
+++ CSharp Web 2018/Csharp MVC Frameworks - ASP.NET Core/Exercise Introduction To ASP.NET CORE/ChushkaToAsp/Chushka.Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿namespace Chushka.Web.Data
+{
+    using Chushka.Data.EntityConfiguration;
+    using Chushka.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext<ChushkaUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=ChushkaDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration<Order>(new OrderConfiguration());
+        }
+    }
+}
---
+++ CSharp Web 2018/Csharp MVC Frameworks - ASP.NET Core/Exercise Introduction To ASP.NET CORE/ChushkaToAsp/Chushka.Data/EntityConfiguration/OrderConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace Chushka.Data.EntityConfiguration
+{
+    using Chushka.Models;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder
+                .HasKey(o => o.Id);
+
+            builder
+                .HasOne(o => o.Client)
+                .WithMany(u => u.Orders)
+                .HasForeignKey(o => o.ClientId);
+
+            builder
+                .HasOne(o => o.Product)
+                .WithMany(p => p.Orders)
+                .HasForeignKey(o => o.ProductId);
+        }
+    }
+}
