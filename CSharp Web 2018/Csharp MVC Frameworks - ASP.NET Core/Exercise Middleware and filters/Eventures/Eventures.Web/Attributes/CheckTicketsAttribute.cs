db5e743efce49e5c395dc1dd1bf04a23497623dc
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2018-11-26 16:10:43 +0200

    Added filters and custom validation attributes.

---
+++ CSharp Web 2018/Csharp MVC Frameworks - ASP.NET Core/Exercise Middleware and filters/Eventures/Eventures.Web/Attributes/CheckTicketsAttribute.cs
@@ -0,0 +1,35 @@
+﻿namespace Eventures.Web.Attributes
+{
+    using Eventures.Models;
+    using Eventures.Web.Services.Events.Contracts;
+    using Eventures.Web.Services.Orders.Contracts;
+    using Eventures.Web.ViewModels.Orders;
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CheckTicketsAttribute : ValidationAttribute
+    {
+        private IEventsService eventsService;
+        private IOrdersService ordersService;
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+
+            this.eventsService = (IEventsService)validationContext.GetService(typeof(IEventsService));
+            this.ordersService = (IOrdersService)validationContext.GetService(typeof(IOrdersService));
+
+            OrderInputModel model = (OrderInputModel)value;
+            Event currentEvent = this.eventsService.GetEvent(model.EventName);
+            
+
+            var currentTicketsCountForEvent = this.eventsService.GetCurrentBoughtTicketsCount(model.EventName);
+            if (!((currentTicketsCountForEvent + model.TicketsCount) >= currentEvent.TotalTickets))
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("Not enough tickets left for event.");
+        }
+    }
+}
---
+++ CSharp Web 2018/Csharp MVC Frameworks - ASP.NET Core/Exercise Middleware and filters/Eventures/Eventures.Web/Filters/LogEventFilter.cs
@@ -0,0 +1,33 @@
+﻿namespace Eventures.Web.Filters
+{
+    using Eventures.Web.Services.Events.Contracts;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Globalization;
+
+    public class LogEventFilter : Attribute, IResultFilter
+    {
+        private readonly ILogger<LogEventFilter> logger;
+
+        public LogEventFilter(ILogger<LogEventFilter> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            string username = context.HttpContext.User.Identity.Name;
+            string eventName = context.HttpContext.Request.Form["Name"];
+            string startDate = context.HttpContext.Request.Form["Start"];
+            string endDate = context.HttpContext.Request.Form["End"];
+
+            logger
+                .LogInformation($"[{DateTime.UtcNow}] Administrator {username} create event {eventName} ({startDate} / {endDate}).");
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+        }
+    }
+}
