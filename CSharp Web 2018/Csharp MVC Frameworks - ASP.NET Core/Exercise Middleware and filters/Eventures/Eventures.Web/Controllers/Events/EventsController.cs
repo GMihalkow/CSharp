65633ac98752194ce2a6f47a9cf395ffcb3fb3fc
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2018-11-26 16:11:33 +0200

    Added all controllers.

---
+++ CSharp Web 2018/Csharp MVC Frameworks - ASP.NET Core/Exercise Middleware and filters/Eventures/Eventures.Web/Controllers/Events/EventsController.cs
@@ -0,0 +1,66 @@
+﻿namespace Eventures.Web.Controllers.Events
+{
+    using Eventures.Web.Filters;
+    using Eventures.Web.Services.Events.Contracts;
+    using Eventures.Web.ViewModels.Events;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
+    public class EventsController : BaseController
+    {
+        private readonly IEventsService eventsService;
+        private readonly ILogger<EventsController> logger;
+
+        public EventsController(IEventsService eventsService, ILogger<EventsController> logger)
+        {
+            this.eventsService = eventsService;
+            this.logger = logger;
+        }
+
+        [Authorize("Admin")]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [TypeFilter(typeof(LogEventFilter))]
+        [Authorize("Admin")]
+        public IActionResult Create(CreateEventInputModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = this.eventsService.AddEvent(model, this.User);
+
+                this.logger.LogInformation($"Event created: {model.Name}");
+               
+                if(result is PageResult)
+                {
+                    result = this.View(model);
+                }
+
+                return result;
+            }
+            else
+            {
+                return this.View(model);
+            }
+        }
+
+        public IActionResult All()
+        {
+            var events = this.eventsService.AllEvents();
+            
+            return this.View(events);
+        }
+
+        public IActionResult My()
+        {
+            var events = this.eventsService.MyEvents(this.User);
+
+            return this.View(events);
+        }
+    }
+}
---
+++ CSharp Web 2018/Csharp MVC Frameworks - ASP.NET Core/Exercise Middleware and filters/Eventures/Eventures.Web/Controllers/Orders/OrdersController.cs
@@ -0,0 +1,41 @@
+﻿namespace Eventures.Web.Controllers.Orders
+{
+    using Eventures.Web.Services.Orders.Contracts;
+    using Eventures.Web.ViewModels.Orders;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class OrdersController : BaseController
+    {
+        private readonly IOrdersService ordersService;
+
+        public OrdersController(IOrdersService ordersService)
+        {
+            this.ordersService = ordersService;
+        }
+        
+        [HttpPost]
+        public IActionResult Order(OrderInputModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                this.ordersService.AddOrder(model.TicketsCount, this.User, model.EventName);
+
+                return this.Redirect("/");
+            }
+            else
+            {
+                return this.Redirect("/Events/All");
+            }
+        }
+
+        [Authorize("Admin")]
+        public IActionResult All()
+        {
+            var orders = this.ordersService.GetAllOrders();
+
+            return this.View(orders);
+        }
+    }
+}
