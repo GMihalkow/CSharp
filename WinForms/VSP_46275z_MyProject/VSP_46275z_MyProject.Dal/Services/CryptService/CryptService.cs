2151b7b8396a7b3f7aa49fc2ca2490a457acc943
GMihalkov <georgimihalkov@abv.bg>
2020-11-07 13:51:41 +0200

    Simplified base64 encryption and decryption.

--- WinForms/VSP_46275z_MyProject/VSP_46275z_MyProject.Dal/Services/CryptService/CryptService.cs
+++ WinForms/VSP_46275z_MyProject/VSP_46275z_MyProject.Dal/Services/CryptService/CryptService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Cryptography;
 using System.Text;
 using VSP_46275z_MyProject.Dal.Services.CryptService.Contracts;
 
@@ -7,35 +6,18 @@ namespace VSP_46275z_MyProject.Dal.Services.CryptService
 {
     public class CryptService : ICryptService
     {
-        private readonly byte[] key = new byte[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
-        private readonly byte[] iv = new byte[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
-
-
         public string Encrypt(string text)
         {
-            using (var algorithm = DES.Create())
-            {
-                var transform = algorithm.CreateEncryptor(this.key, this.iv);
+            var inputbuffer = Encoding.Unicode.GetBytes(text);
 
-                var inputbuffer = Encoding.Unicode.GetBytes(text);
-                var outputBuffer = transform.TransformFinalBlock(inputbuffer, 0, inputbuffer.Length);
-
-                return Convert.ToBase64String(outputBuffer);
-            }
+            return Convert.ToBase64String(inputbuffer);
         }
 
         public string Decrypt(string text)
         {
-            using (var algorithm = DES.Create())
-            {
-                var transform = algorithm.CreateDecryptor(this.key, this.iv);
+            var resultBuffer = Convert.FromBase64String(text);
 
-                var inputbuffer = Convert.FromBase64String(text);
-                var outputBuffer = transform.TransformFinalBlock(inputbuffer, 0, inputbuffer.Length);
-
-                return Encoding.Unicode.GetString(outputBuffer);
-
-            }
+            return Encoding.Unicode.GetString(resultBuffer);
         }
     }
 }
